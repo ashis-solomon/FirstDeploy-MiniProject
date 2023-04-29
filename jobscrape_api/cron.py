11d696a119370b20c1b01d0bcdd9a8128cd67459
ashis-solomon <ashissolomon24@gmail.com>
2023-04-30 00:19:38 +0530

    update

--- jobscrape_api/cron.py
+++ jobscrape_api/cron.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+import time
 from jobscrape_api.models import ScrapeJob
 
 def my_scheduled_job():
     print(f'CRRROOOONNN!! {datetime.now()}')
     print('----------------------------------------------')
-    job = ScrapeJob(job_name=str(datetime.now().strftime('%H:%M:%S')))
-    job.save()
+    while(True):
+        time.sleep(20)
+        job = ScrapeJob(job_name=str(datetime.now().strftime('%H:%M:%S')))
+        job.save()
